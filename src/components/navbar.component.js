7e3f17991fcf8fa0f266805a1ea6b75e2c52ff60
saurabhsahugit <saurabh.newhorizons@gmail.com>
2023-01-18 10:56:51 +0000

    FE nearly finsihed, react-datepicker having some compatibility issues

--- src/components/navbar.component.js
+++ src/components/navbar.component.js
@@ -7,18 +7,18 @@ export default class Navbar extends Component {
     return (
       <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
         <Link to="/" className="navbar-brand">ExcerTracker</Link>
-        <div className="collpase navbar-collapse">
-        <ul className="navbar-nav mr-auto">
-          <li className="navbar-item">
-          <Link to="/" className="nav-link">Exercises</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/create" className="nav-link">Create Exercise Log</Link>
-          </li>
-          <li className="navbar-item">
-          <Link to="/user" className="nav-link">Create User</Link>
-          </li>
-        </ul>
+        <div className="collapse navbar-collapse">
+          <ul className="navbar-nav mr-auto">
+            <li className="navbar-item">
+              <Link to="/" className="nav-link">Exercises</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/create" className="nav-link">Create Exercise Log</Link>
+            </li>
+            <li className="navbar-item">
+              <Link to="/user" className="nav-link">Create User</Link>
+            </li>
+          </ul>
         </div>
       </nav>
     );
